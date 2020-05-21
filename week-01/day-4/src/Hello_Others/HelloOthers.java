89b96ea037f824da596affa2f32ec3a84a232abf
Szid <amszideri@gmail.com>
2020-05-21 15:19:22 +0200

    Hello_Others

---
+++ week-01/day-4/src/Hello_Others/HelloOthers.java
@@ -0,0 +1,10 @@
+package Hello_Others;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Eszter!");
+        System.out.println("Hello, Aron!");
+        System.out.println("Hello, Emi!");
+       
+    }
+}
