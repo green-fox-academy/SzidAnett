c19c080a57bf849cf096540ee5a988fdd741673c
Szid <amszideri@gmail.com>
2020-05-22 19:14:39 +0200

    iWontCheatOnTheExams

---
+++ week-01/day-5/src/i_wont_cheat_on_the_exams/IWontCheatOnTheExams.java
@@ -0,0 +1,15 @@
+package i_wont_cheat_on_the_exams;
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+public class IWontCheatOnTheExams {
+    public static void main (String []args) {
+        int i = 1;
+        while (i < 101){
+            System.out.println(i + "." + " I won't cheat on the exam!");
+            i++;
+        }
+
+    }
+}
