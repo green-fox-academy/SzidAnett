75ccfac57df41c00bfd99a8a968e0f53643ade9f
Szid <amszideri@gmail.com>
2020-05-25 20:34:47 +0200

    anagram

---
+++ week-02/day-1/Functions/src/anagram/Anagram.java
@@ -0,0 +1,30 @@
+package anagram;
+import java.util.Scanner;
+public class Anagram {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Plz, give me two words that you want to check if they anagrams. First word: ");
+            String firstWord = scanner.nextLine();
+
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("Plz, give me the second word: ");
+            String secondWord = scanner1.nextLine();
+
+            System.out.println(anagram(firstWord,secondWord));
+
+        }
+        public static boolean anagram(String firstWord,String secondWord){
+            String dogGod ="";
+            for (int i = secondWord.length(); i > 0 ; i--) {
+                 dogGod += secondWord.charAt(i-1);
+            }
+            if(dogGod.equals(firstWord)){
+                return true;
+
+            }else{
+                return false;
+
+            }
+        }
+}
