9ac7e4960263c1e9084647119307590784adfba6
Szid <amszideri@gmail.com>
2020-05-28 17:32:35 +0200

    fourRectangles

---
+++ week-02/day-4/src/four_rectangles/FourRectangles.java
@@ -0,0 +1,49 @@
+package four_rectangles;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+public class FourRectangles {
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+    }
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+            Graphics2D g2 = (Graphics2D) graphics;
+            g2.setStroke(new BasicStroke(3));
+
+            Random random = new Random();
+
+            for (int i = 0; i < 4; i++) {
+                int r = random.nextInt(256);
+                int g = random.nextInt(256);
+                int b = random.nextInt(256);
+                graphics.setColor(new Color(r, g, b));
+
+                int x = random.nextInt(320);
+                int y = random.nextInt(320);
+                int z = random.nextInt(320 - x);
+                int w = random.nextInt(320 - y);
+                graphics.fillRect(x, y, z, w);
+        }
+    }
+}
+}
