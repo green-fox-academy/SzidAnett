b6f1dff3b27839453948e5c404b3f14fc8353f1a
Szid <amszideri@gmail.com>
2020-06-03 09:43:02 +0200

    HomeWork

---
+++ week-03/day-2/src/home_work/HomeWork.java
@@ -0,0 +1,38 @@
+package home_work;
+
+import com.sun.source.tree.Tree;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        String tree1 =("");
+        String tree1type = "Bonsai Sakura";
+        String tree1leafColor = "green";
+        int tree1age = 150;
+        boolean tree1male = true;
+
+        String tree2 =("");
+        String tree2type = "Oak";
+        String tree2leafColor = "green";
+        int tree2age = 65;
+        boolean tree2male = true;
+
+        String tree3 =("");
+        String tree3type = "Walnuts";
+        String tree3leafColor = "green";
+        int tree3age = 28;
+        boolean tree3male = true;
+
+        String tree4 =("");
+        String tree4type = "Pine";
+        String tree4leafColor = "green";
+        int tree4age = 92;
+        boolean tree4male = true;
+
+        String tree5 =("");
+        String tree5type = "Plums";
+        String tree5leafColor = "green";
+        int tree5age = 18;
+        boolean tree5male = true;
+    }
+}
+
