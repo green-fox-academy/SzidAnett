e885844e8f586fbb95dfec3a9603f1560b61812f
Szid <amszideri@gmail.com>
2020-06-05 18:05:16 +0200

    modified sum

--- week-03/day-5/Test/sum/SumTest.java
+++ week-03/day-5/Test/sum/SumTest.java
@@ -2,15 +2,42 @@ package sum;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SumTest {
-
     @Test
-    void main() {
-        assertEquals();
+    public void test1() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Sum.fill(numbers);
+        assertEquals(55, Sum.findSum(numbers));
     }
 
-    private void assertEquals() {
+    @Test
+    public void test2() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        assertEquals(0, Sum.findSum(numbers));
+    }
+
+    @Test
+    public void test3() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        assertEquals(2, Sum.findSum(numbers));
+    }
+
+    @Test
+    public void test4() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Sum.fill(numbers);
+        assertEquals(55, Sum.findSum(numbers));
+    }
+
+    @Test
+    public void test5() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(null);
+
     }
 }
--- week-03/day-5/src/sum/Sum.java
+++ week-03/day-5/src/sum/Sum.java
@@ -1,23 +1,37 @@
 package sum;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Sum {
+    public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>();
 
-    public static int main (String []args){
-        ArrayList<Integer> sum = new ArrayList<Integer>();
+    fill(numbers);
+    System.out.println(
 
-        sum.add(8);
-        sum.add(9);
-        sum.add(16);
-        sum.add(13);
-        sum.add(61);
-        sum.add(45);
-        sum.add(87);
+    findSum(numbers));
+}
+    private List<Integer> list;
+    public Sum(){
+        list = new ArrayList<Integer>();
+    }
+    int i = 0;
 
-
-        return sum.size();
-
+    public static int findSum(ArrayList<Integer> green) {
+        int sum = 0;
+        for (int value : green) {
+            sum += value;
+        }
+        return sum;
     }
 
+    public static void fill(ArrayList<Integer> arr) {
+        for (int i = 0; i < 10; i++) {
+            arr.add(i + 1);
+        }
+
+    }
 }
+
+
