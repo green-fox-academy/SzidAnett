7ff99bcd9972873b2a60c64db24524a69d61e9b1
Szid <amszideri@gmail.com>
2020-06-05 15:06:54 +0200

    anagram

---
+++ week-03/day-5/Test/anagram/AnagramTest.java
@@ -0,0 +1,20 @@
+package anagram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnagramTest {
+
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void anagram() {
+        assertFalse(false);
+        assertTrue(true);
+    }
+
+}
