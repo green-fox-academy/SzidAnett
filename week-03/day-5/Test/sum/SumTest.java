6777bb378af91c24ae3542c2f148a1ef993da634
Szid <amszideri@gmail.com>
2020-06-05 14:46:06 +0200

    sum

---
+++ week-03/day-5/Test/sum/SumTest.java
@@ -0,0 +1,16 @@
+package sum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumTest {
+
+    @Test
+    void main() {
+        assertEquals();
+    }
+
+    private void assertEquals() {
+    }
+}
---
+++ week-03/day-5/src/sum/Sum.java
@@ -0,0 +1,23 @@
+package sum;
+
+import java.util.ArrayList;
+
+public class Sum {
+
+    public static int main (String []args){
+        ArrayList<Integer> sum = new ArrayList<Integer>();
+
+        sum.add(8);
+        sum.add(9);
+        sum.add(16);
+        sum.add(13);
+        sum.add(61);
+        sum.add(45);
+        sum.add(87);
+
+
+        return sum.size();
+
+    }
+
+}
